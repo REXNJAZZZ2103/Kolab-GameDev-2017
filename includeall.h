a74256c3f8ec72c4ebd95d1c1c1a48763e8356f2
Tony <buddy90_lost@yahoo.co.id>
2017-11-16 19:46:36 +0700

    Create includeall.h

---
+++ includeall.h
@@ -0,0 +1,17 @@
+#ifndef _INCLUDE_ALL_H
+
+#define _INCLUDE_ALL_H
+
+#include "boolean.h"
+#include "map.h"
+#include "listsirkuler.h"
+#include "matriks.h"
+#include "unit.h"
+#include "mesinkata.h"
+#include "pcolor.h"
+#include "queue.h"
+#include "stackpoint.h"
+#include "point.h"
+#include "player.h"
+
+#endif
