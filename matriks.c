8db15091d4fce812a40f212ad0d6f942e4803e5c
AgeMagi <32269979+AgeMagi@users.noreply.github.com>
2017-11-17 15:29:43 +0700

    Update matriks.c

--- matriks.c
+++ matriks.c
@@ -1,5 +1,6 @@
 /* ********** Definisi TYPE MATRIKS dengan indeks dan elemen integer ********** */
 #include "matriks.h"
+#include <stdio.h>
 
 /* Ukuran minimum dan maksimum baris dan kolom */
 #define BrsMin 1
@@ -40,8 +41,8 @@ void CopyMATRIKS (MATRIKS MIn, MATRIKS * MHsl)
 /* Melakukan assignment MHsl  MIn */
 {
     int i,j;
-    for (i = GetFirstIdxBrs(MIn); i <= GetLastIdxBrs(MIn); i++) {
-        for (j = GetFirstIdxKol(MIn); j <= GetLastIdxKol(MIn); j++) {
+    for (i = 1; i <= MIn.NBrsEff; i++) {
+        for (j = 1; j <= MIn.NKolEff; j++) {
             Elmt(*MHsl,i,j) = Elmt(MIn,i,j);
         }
     }
@@ -49,61 +50,3 @@ void CopyMATRIKS (MATRIKS MIn, MATRIKS * MHsl)
     NBrsEff(*MHsl) = NBrsEff(MIn);
     NKolEff(*MHsl) = NKolEff(MIn);
 }
-/* ********** KELOMPOK BACA/TULIS ********** */
-void BacaMATRIKS (MATRIKS * M, int NB, int NK)
-/* I.S. IsIdxValid(NB,NK) */
-/* F.S. M terdefinisi nilai elemen efektifnya, berukuran NB x NK */
-/* Proses: Melakukan MakeMATRIKS(M,NB,NK) dan mengisi nilai efektifnya */
-/* Selanjutnya membaca nilai elemen per baris dan kolom */
-/* Contoh: Jika NB = 3 dan NK = 3, maka contoh cara membaca isi matriks :
-1 2 3
-4 5 6
-8 9 10
-*/
-{
-    int i,j;
-
-    MakeMATRIKS(NB,NK,&M);
-    NBrsEff(*M) = NB;
-    NKolEff(*M) = NK;
-
-    for (i = GetFirstIdxBrs(*M); i <= GetLastIdxBrs(*M); i++) {
-        for (j = GetFirstIdxKol(*M); j <= GetLastIdxKol(*M); j++) {
-            scanf("&d",&Elmt(*M,i,j));
-        }
-    }
-}
-
-void TulisMATRIKS (MATRIKS M)
-/* I.S. M terdefinisi */
-/* F.S. Nilai M(i,j) ditulis ke layar per baris per kolom, masing-masing elemen per baris
-   dipisahkan sebuah spasi */
-/* Proses: Menulis nilai setiap elemen M ke layar dengan traversal per baris dan per kolom */
-/* Contoh: menulis matriks 3x3 (ingat di akhir tiap baris, tidak ada spasi)
-1 2 3
-4 5 6
-8 9 10
-*/
-{
-    int i,j;
-	for (i = GetFirstIdxBrs(M); i <= GetLastIdxBrs(M); i++)
-	{
-		for (j = GetFirstIdxKol(M); j <= GetLastIdxKol(M); j++)
-		{
-			printf("%d", Elmt(M, i, j));
-			if (j != GetLastIdxKol(M) )
-			{
-				printf(" ");
-			}
-			else
-			{
-				if (i != GetLastIdxBrs(M))
-				{
-					printf("\n");
-				}
-			}
-		}
-	}
-}
-
-
